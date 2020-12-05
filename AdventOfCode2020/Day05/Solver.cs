ccfe041040eb7d96e59f0c735ed6f680c6dd3c5b
PROD\GFulep <Gabor.Fulep@quest.com>
2020-12-05 11:20:38 +0100

    Solve day 5 part two

--- AdventOfCode2020/Day05/Solver.cs
+++ AdventOfCode2020/Day05/Solver.cs
@@ -6,17 +6,28 @@ namespace AdventOfCode2020.Day05
 {
     public class Solver : ISolver
     {
-        private readonly IEnumerable<BoardingPass> boardingPasses;
+        private readonly List<int> boardingPassIds;
 
         public Solver(IEnumerable<BoardingPass> boardingPasses)
         {
-            this.boardingPasses = boardingPasses;
+            this.boardingPassIds = boardingPasses
+                .OrderByDescending(bp => bp.SeatId)
+                .Select(bp => bp.SeatId)
+                .ToList();
         }
-        public string GetFirstSolution() => boardingPasses.Max(bp => bp.SeatId).ToString();
+        public string GetFirstSolution() => boardingPassIds.FirstOrDefault().ToString();
 
         public string GetSecondSolution()
         {
-            return "";
+            int mySeat = -1;
+            for (int i = 0; i < boardingPassIds.Count - 1 && mySeat == -1; i++)
+            {
+                if (boardingPassIds[i] - boardingPassIds[i + 1] > 1)
+                {
+                    mySeat = boardingPassIds[i] - 1;
+                }
+            }
+            return mySeat.ToString();
         }
     }
 }
